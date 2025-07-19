09d2398b1cc8388666f178689f6ea5f44809fbba
Carlos3443 <151295984+Carlos3443@users.noreply.github.com>
2025-07-19 18:57:21 -0500

    Delete EcoEats directory

--- EcoEats/backend/recipes/views.py
+++
@@ -1,9 +0,0 @@
-# recipes/views.py
-
-from rest_framework import viewsets
-from .models import Recipe
-from .serializers import RecipeSerializer
-
-class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
